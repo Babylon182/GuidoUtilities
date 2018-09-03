f0136a6d0f1bb7ca209882e1043402f2d7bee47e
Babylon182 <guido_calonge@hotmail.com>
2018-09-02 21:46:17 -0300

    -Improvement to the GodPool - Unity Version upgraded

--- Assets/Scripts/ObjectsPool/GodPool.cs
+++ Assets/Scripts/ObjectsPool/GodPool.cs
@@ -11,7 +11,7 @@ namespace ObjectsPool
 		
 		[SerializeField] private PoolAmount[] initialPoolObjects;
 		private Dictionary<string, Stack<PoolObject>> inactivePoolObjects;
-		private Dictionary<string, List<PoolObject>> activePoolObjects;
+		private Dictionary<string, Dictionary<int, PoolObject>> activePoolObjects;
 		private Dictionary<string, GameObject> containers;
 
 		private const string CONTAINER = "Container";
@@ -22,7 +22,7 @@ namespace ObjectsPool
 			{
 				instance = this;
 				inactivePoolObjects = new Dictionary<string, Stack<PoolObject>>();
-				activePoolObjects = new Dictionary<string, List<PoolObject>>();
+				activePoolObjects = new Dictionary<string, Dictionary<int, PoolObject>>();
 				containers = new Dictionary<string, GameObject>();
 				CreateInitialPoolObjects();
 			}
@@ -34,17 +34,7 @@ namespace ObjectsPool
 
 		public GameObject InstantiatePoolObject(GameObject poolObjectType, Vector3 initialPosition, Quaternion intialRotation)
 		{
-			if (!inactivePoolObjects.ContainsKey(poolObjectType.name))
-			{
-				string poolObjectName = poolObjectType.name;
-				inactivePoolObjects.Add(poolObjectName, new Stack<PoolObject>());
-				activePoolObjects.Add(poolObjectName, new List<PoolObject>());
-				
-				GameObject newContainer = new GameObject(poolObjectName + CONTAINER);
-				newContainer.transform.parent = transform;
-				containers.Add(poolObjectName, newContainer);
-			}
-
+			CheckContainers(poolObjectType);
 			GameObject poolObject = GetObjectFromPool(poolObjectType).PoolGameObject;
 			poolObject.transform.position = initialPosition;
 			poolObject.transform.rotation = intialRotation;
@@ -54,22 +44,15 @@ namespace ObjectsPool
 
 		public void ReturnPoolObject(GameObject poolObjectType)
 		{
-			if (activePoolObjects.ContainsKey(poolObjectType.name))
-			{
-				List<PoolObject> listOfActiveGameobjects = activePoolObjects[poolObjectType.name];
-				
-				for (int i = listOfActiveGameobjects.Count - 1; i >= 0; i--)
-				{
-					if (listOfActiveGameobjects[i].PoolGameObject.Equals(poolObjectType))
-					{
-						PoolObject poolObject = listOfActiveGameobjects[i];
-						poolObject.IsActive = false;
-						
-						listOfActiveGameobjects.Remove(poolObject);
-						inactivePoolObjects[poolObjectType.name].Push(poolObject);
-					}
-				}
-			}
+			if (!activePoolObjects.ContainsKey(poolObjectType.name))
+				return;
+			
+			Dictionary<int, PoolObject> dictionaryOfActiveGameobjects = activePoolObjects[poolObjectType.name];
+			PoolObject poolObject = dictionaryOfActiveGameobjects[poolObjectType.GetInstanceID()];
+			poolObject.IsActive = false;
+			
+			dictionaryOfActiveGameobjects.Remove(poolObject.Id);
+			inactivePoolObjects[poolObjectType.name].Push(poolObject);	
 		}
 
 		private PoolObject GetObjectFromPool(GameObject poolObjectType)
@@ -81,7 +64,7 @@ namespace ObjectsPool
 				PoolObject poolObject = stackOfInactiveGameobjects.Pop();
 				poolObject.IsActive = true;
 				
-				activePoolObjects[poolObjectType.name].Add(poolObject);	
+				activePoolObjects[poolObjectType.name].Add(poolObject.Id, poolObject);	
 				return poolObject;
 			}
 			else
@@ -92,12 +75,12 @@ namespace ObjectsPool
 	
 				PoolObject newPoolObject = CreateGenericPoolObject(newGameObject);
 				newPoolObject.IsActive = true;
-				activePoolObjects[poolObjectType.name].Add(newPoolObject);
+				activePoolObjects[poolObjectType.name].Add(newPoolObject.Id, newPoolObject);
 				
 				return newPoolObject;	
 			}
 		}
-		
+
 		private PoolObject CreateGenericPoolObject(GameObject poolObjectType)
 		{
 			IPoolable poolInterface = poolObjectType.GetComponent<IPoolable>();
@@ -110,37 +93,41 @@ namespace ObjectsPool
 			onDispose += () => poolInterface?.Dispose(); 
 			onDispose += () => poolObjectType.SetActive(false);
 
-			return new PoolObject(poolObjectType, onInit, onDispose);;
+			return new PoolObject(poolObjectType, onInit, onDispose);
+		}
+
+		private void CheckContainers(GameObject poolObjectType)
+		{
+			if (inactivePoolObjects.ContainsKey(poolObjectType.name))
+				return;
+			
+			string poolObjectName = poolObjectType.name;
+			inactivePoolObjects.Add(poolObjectName, new Stack<PoolObject>());
+			activePoolObjects.Add(poolObjectName, new Dictionary<int, PoolObject>());
+			
+			GameObject newContainer = new GameObject(poolObjectName + CONTAINER);
+			newContainer.transform.parent = transform;
+			containers.Add(poolObjectName, newContainer);	
 		}
 
 		private void CreateInitialPoolObjects()
 		{
-			List<string> createdPoolObjectsNames = new List<string>();
 			for (int i = initialPoolObjects.Length - 1; i >= 0; i--)
 			{
-				PoolAmount poolObject = initialPoolObjects[i];
-				createdPoolObjectsNames.Add(poolObject.gameObject.name);
-				for (int j = poolObject.ammount; j > 0; j--)
+				PoolAmount poolAmount = initialPoolObjects[i];
+				GameObject poolGameObject = poolAmount.gameObject;
+				CheckContainers(poolGameObject);
+				for (int j = poolAmount.ammount; j > 0; j--)
 				{
-					InstantiatePoolObject(poolObject.gameObject, Vector3.zero, Quaternion.identity);
+					GameObject newGameObject = Instantiate(poolGameObject, Vector3.zero, Quaternion.identity);
+					newGameObject.name = poolGameObject.name;
+					newGameObject.transform.parent = containers[newGameObject.name].transform;
+					
+					PoolObject newPoolObject = CreateGenericPoolObject(newGameObject);
+					newPoolObject.IsActive = false;
+					inactivePoolObjects[poolGameObject.name].Push(newPoolObject);
 				}
 			}
-
-			for (int i = createdPoolObjectsNames.Count - 1; i >= 0; i--)
-			{
-				string poolObjectName = createdPoolObjectsNames[i];
-				List<PoolObject> listOfActiveGameObjects = activePoolObjects[poolObjectName];
-				
-				for (int j = listOfActiveGameObjects.Count - 1; j >= 0; j--)
-				{
-					PoolObject poolObject = listOfActiveGameObjects[j];
-					poolObject.IsActive = false;
-						
-					listOfActiveGameObjects.Remove(poolObject);
-					inactivePoolObjects[poolObjectName].Push(poolObject);
-				}	
-			}
-				
 			initialPoolObjects = null;
 		}
 	}
@@ -156,9 +143,11 @@ namespace ObjectsPool
 			this.PoolGameObject = poolGameObject;
 			this.initCallback = initCallback;
 			this.disposeCallback = disposeCallback;
+			Id = poolGameObject.GetInstanceID();
 		}
 
 		public GameObject PoolGameObject { get;}
+		public int Id { get;}
 
 		public bool IsActive
 		{
